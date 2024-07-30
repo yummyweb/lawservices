85bf319a7bc4f0392f5a8d04e99025bb78d652c6
yummyweb <antriksh.timepass@gmail.com>
2024-07-30 10:42:36 +0530

    decent

--- src/components/ReviewForm.jsx
+++ src/components/ReviewForm.jsx
@@ -1,41 +1,62 @@
+import { useRef } from "react";
 import "./ReviewForm.css"
+import emailjs from '@emailjs/browser'
 
 function ReviewForm() {
+    const form = useRef()
+
+    const sendEmail = e => {
+        e.preventDefault();
+
+        emailjs
+            .sendForm('service_okprt9e', 'template_z9e6w8f', form.current, {
+                publicKey: 'LtuoglxrmioZ7WIKU',
+            })
+            .then(
+                () => {
+                    console.log('SUCCESS!');
+                },
+                (error) => {
+                    console.log('FAILED...', error.text);
+                },
+            );
+    }
+
     return (
         <div className="form-wrapper">
             <h1 className="font-title">Let's talk about your story.</h1>
-            <form>
+            <form ref={form} onSubmit={sendEmail}>
                 <div className="form-field">
                     <label for="name">Name</label>
-                    <input id="name" />
+                    <input name="client_name" id="name" />
                 </div>
 
                 <div className="form-field">
                     <label for="email">Email</label>
-                    <input id="email" />
+                    <input name="client_email" id="email" />
                 </div>
 
                 <div className="form-field">
                     <label for="phone">Phone</label>
-                    <input id="phone" />
+                    <input name="client_phone" id="phone" />
                 </div>
 
                 <div className="form-field">
                     <label for="city">City</label>
-                    <input id="city" />
+                    <input name="client_city" id="city" />
                 </div>
 
                 <div className="form-field">
                     <label for="state">State</label>
-                    <input id="state" />
+                    <input name="client_state" id="state" />
                 </div>
 
                 <div className="form-field">
                     <label for="nature">Nature of Matter</label>
-                    <input id="nature" />
+                    <input name="client_nature" id="nature" />
                 </div>
 
-                <button onClick={e => e.preventDefault()} className="primary">Submit</button>
+                <button className="primary">Submit</button>
             </form>
         </div>
     )
