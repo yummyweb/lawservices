71df7d8432d9b9c122da929a57f96f79f8d83fd0
yummyweb <antriksh.timepass@gmail.com>
2024-08-06 21:35:49 +0530

    all

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,7 +7,7 @@ function Hero() {
                 <p className="badge color-primary">Law firm</p>
                 <h1 className="heading font-title">Overcome legal <br /> obstacles to <br /> your <span className="color-primary font-title">success!</span></h1>
                 <p className="subheading color-mute">Law without obstacles for your success. Professional support to help you overcome legal difficulties and achieve your goals.</p>
-                <button onClick={() => window.open("https://calendly.com/mailfornishantverma")} className="primary">Book a free consultation</button>
+                <button onClick={() => window.open("https://calendly.com/contact-nishantbharihoke")} className="primary">Book a free consultation</button>
             </div>
             <div className="right">
                 <div className="img-container">
